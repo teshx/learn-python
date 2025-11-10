a90d6b34aa3cc36b763bc6568c15eaf0afd6d49b
teshager habtie <teshie00001@gmail.com>
2025-11-10 07:15:15 -0600

    this resive input from user

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
-database_name="school"
-table_name="student"
-print(database_name ,table_name ,sep="\n")
+# database_name="school"
+# table_name="student"
+# print(database_name ,table_name ,sep="\n")
+
+database_name = input("Please enter the database name: ")
+school_name = input("Please enter the school name: ")
+colomon_number = int(input("Please enter the colomon number: "))
+
+print(database_name , school_name , colomon_number , sep='\n')
+print(colomon_number +1 , sep='\n')
