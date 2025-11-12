83a14be2afe10ac30a2cc4df64cbf073f9051386
teshager habtie <teshie00001@gmail.com>
2025-11-12 12:31:33 -0600

    working on if conditionals and loops

--- second.py
+++ second.py
@@ -25,3 +25,44 @@ print(c)
 
 x = {1,2,3,4,5,3,2}
 print(x)
+# condtions
+if 5 > 6:
+    print("Five is greater than two!")
+else:
+    print("Two is greater than five!")
+# loops
+
+
+v=15
+if v < 2:
+    print("Value is less than 2")
+elif v == 5:
+    print("Value is equal to 5")
+else:
+    print("Value is greater than or equal to 2 and not equal to 5")
+
+
+#check even odd
+# num = int(input("Enter a number: "))
+# print("You entered:", num)
+# if num % 2 == 0:
+#     print("The number is even.")
+# elif num%2==1:
+#     print("num is odd")
+# else:
+#     print("num is zero")
+
+# for loop
+for i in range(10, 50):
+    print(i)
+for i in range(100, 50 ,-1):
+    print(i)
+
+#combine for loop and if
+
+for i in range(1 , 100):
+    if i%3==0:
+        print(i,"fizz")
+    elif i%5==0:
+        print(i, "buzz")
+    else: print("number", i)
