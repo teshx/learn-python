86f4e68087c78628eba2f5491e39d301fe49d6cf
teshager habtie <teshie00001@gmail.com>
2025-11-12 10:40:46 -0600

    added second file

--- first.py
+++ first.py
@@ -2,9 +2,14 @@
 # table_name="student"
 # print(database_name ,table_name ,sep="\n")
 
+# database_name = input("Please enter the database name: ")
+# school_name = input("Please enter the school name: ")
+# colomon_number = int(input("Please enter the colomon number: "))
+#
+# print(database_name , school_name , colomon_number , sep='\n')
+# print(colomon_number +1 , sep='\n')
+# print(f"The number is {colomon_number} and database name is {database_name} and school name is {school_name}")
 database_name = input("Please enter the database name: ")
-school_name = input("Please enter the school name: ")
-colomon_number = int(input("Please enter the colomon number: "))
-
-print(database_name , school_name , colomon_number , sep='\n')
-print(colomon_number +1 , sep='\n')
+table_name = input("Please enter the table name: ")
+num_colomon = int(input("Please enter the colomon number: "))
+print(f"database \"{database_name}\" created successfully with a table named \"{table_name}\" containing {num_colomon} colomons")
