3d4678b2b4070a1197a447a1a6ef8bbf2e5feb79
teshager habtie <teshie00001@gmail.com>
2025-11-13 12:16:10 -0600

    working on dictionary and set

--- list-unpacking.py
+++ list-unpacking.py
@@ -1,13 +1,40 @@
-import sys
+# import sys
 
-arguments = path, command_name, db_name = sys.argv # this is called list unpacking in python
-print(arguments)
-print('''
-You can print anything here.
-You can even add a multiple lines 
-''')
+# arguments = path, command_name, db_name = sys.argv # this is called list unpacking in python
+# print(arguments)
+# print('''
+# You can print anything here.
+# You can even add a multiple lines 
+# ''')
 
 # path, arg1, arg2 = sys.argv
 # print(sys.argv)
 # print(f'Path: {path} arg1: {arg1} arg2: {arg2}')
 # print(f' The command is {command_name.capitalize()} and the db name is {db_name.upper()} ')
+
+#dictionary
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(car)
+
+print(car["brand"]) 
+
+for key in car:
+    print(key, car[key])
+
+if "brand" in car:
+    print(f"the brand is {car["brand"]}")
+
+if "name" in car:
+    print(f"the name is {car["name"]}")
+else:
+    print("the name is does not exist")
+
+
+if "name" not in car:
+    print("The name is does not exist!")
---
+++ set.py
@@ -0,0 +1,3 @@
+letter1 = {'a', 'b', 'c', 'd', 'e', 'f' }
+del letter1
+print(letter1)
